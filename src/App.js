1e0d56830c8d0db4ef7cb0967b58be11f7699014
terfiel-22 <dietherfiel@gmail.com>
2023-08-04 22:55:19 +0800

    Convert Class Components to Functional Components

--- src/App.js
+++ src/App.js
@@ -1,62 +1,107 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Component } from "react";
 import SearchBox from "./components/search-box/SearchBox";
 import CardList from "./components/card-list/CardList";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
-    this.state = {
-      users: [],
-      isLoading: true,
-      searchString: "",
-    };
-  }
+  useEffect(() => {
+    fetch("https://dummyjson.com/users?limit=10")
+      .then((res) => res.json())
+      .then((res) => setUsers(res.users))
+      .then(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-  componentDidMount() {
-    fetch("https://dummyjson.com/users?limit=10").then((res) =>
-      res.json().then((res) =>
-        this.setState({
-          users: res.users,
-          isLoading: false,
-        })
-      )
-    );
-  }
-
-  onSearchChange = (e) => {
-    const searchString = e.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchString };
-    });
-  };
-
-  render() {
-    const { users, isLoading, searchString } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredUsers = users.filter((user) => {
+  useEffect(() => {
+    const newFilteredUsers = users.filter((user) => {
       const fullName =
         user.firstName.toLocaleLowerCase() + user.lastName.toLocaleLowerCase();
-      return fullName.includes(searchString);
+      return fullName.includes(searchField);
     });
+    setFilteredUsers(newFilteredUsers);
+  }, [users, searchField]);
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="app-title">User Rolodex</h1>
-          <SearchBox
-            className="user-search-box"
-            placeholder="Search user"
-            onChangeHandler={onSearchChange}
-          />
+  const onSearchChange = (e) => {
+    const searchString = e.target.value.toLowerCase();
+    setSearchField(searchString);
+  };
 
-          <CardList users={filteredUsers} isLoading={isLoading} />
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="app-title">User Rolodex</h1>
+        <SearchBox
+          className="user-search-box"
+          placeholder="Search user"
+          onChangeHandler={onSearchChange}
+        />
+
+        <CardList users={filteredUsers} isLoading={isLoading} />
+      </header>
+    </div>
+  );
+};
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       users: [],
+//       isLoading: true,
+//       searchString: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://dummyjson.com/users?limit=10").then((res) =>
+//       res.json().then((res) =>
+//         this.setState({
+//           users: res.users,
+//           isLoading: false,
+//         })
+//       )
+//     );
+//   }
+
+// onSearchChange = (e) => {
+//   const searchString = e.target.value.toLowerCase();
+//   this.setState(() => {
+//     return { searchString };
+//   });
+// };
+
+//   render() {
+//     const { users, isLoading, searchString } = this.state;
+//     const { onSearchChange } = this;
+
+// const filteredUsers = users.filter((user) => {
+//   const fullName =
+//     user.firstName.toLocaleLowerCase() + user.lastName.toLocaleLowerCase();
+//   return fullName.includes(searchString);
+// });
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <h1 className="app-title">User Rolodex</h1>
+//           <SearchBox
+//             className="user-search-box"
+//             placeholder="Search user"
+//             onChangeHandler={onSearchChange}
+//           />
+
+//           <CardList users={filteredUsers} isLoading={isLoading} />
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
 import "./card.styles.css";
-class Card extends Component {
-  render() {
-    const { user } = this.props;
-    const { firstName, lastName, image, email } = user;
-    return (
-        <div className="card-container">
-            <img alt={`User ${firstName} ${lastName}`} src={image} />
-            <h3>{firstName + " " + lastName}</h3>
-            <p>{email}</p>
-        </div>
-    );
-  }
+
+const Card = ({ user }) => {
+  const { firstName, lastName, image, email } = user;
+  return (
+      <div className="card-container">
+          <img alt={`User ${firstName} ${lastName}`} src={image} />
+          <h3>{firstName + " " + lastName}</h3>
+          <p>{email}</p>
+      </div>
+  );
 }
 
 export default Card;
